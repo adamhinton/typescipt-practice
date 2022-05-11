8cd2b3e759d356882587acd3ae4d4d9395643128
adamhinton <adam.d.hinton@gmail.com>
2022-05-10 23:07:42 -0400

    exporting my first interface from within a namespace

--- src/drag-drop-interfaces.ts
+++ src/drag-drop-interfaces.ts
@@ -3,12 +3,12 @@
 // you can put anything in a namespace: classes, constants, anything.
 //can export items from within namespaces, very cool.
 namespace DDInterfaces {
-  interface Draggable {
+  export interface Draggable {
     dragStartHandler(event: DragEvent): void;
     dragEndHandler(event: DragEvent): void;
   }
 
-  interface DragTarget {
+  export interface DragTarget {
     dragOverHandler(event: DragEvent): void;
     dropHandler(event: DragEvent): void;
     dragLeaveHandler(event: DragEvent): void;
