d95653af2baab0368fb0ecace54e4f2ab14fa2f7
adamhinton <adam.d.hinton@gmail.com>
2022-05-14 14:10:18 -0400

    fix import errors due to new file names

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 //you add three slashes to import from namespace
 //write self closing xml tag and file name as reference path
-/// <reference path = "drag-drop-interfaces.ts"/>
-///<reference path = "project-model.ts"/>
-///<reference path = "project-state.ts"/>
-///<reference path = "autobind-decorator.ts"/>
+/// <reference path = "models/drag-drop.ts"/>
+///<reference path = "models/project.ts"/>
+///<reference path = "state/project-state.ts"/>
+///<reference path = "decorators/autobind.ts"/>
 // This means we tell TS where to find this info
 
 namespace App {
