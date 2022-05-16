ebb3d7aa13cab08915930dcccabd6213e3e7f9d8
adamhinton <adam.d.hinton@gmail.com>
2022-05-15 23:54:22 -0400

    del unneeded comments

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,7 +1,5 @@
 // Drag & Drop Interfaces
 
-// you can put anything in a namespace: classes, constants, anything.
-//can export items from within namespaces, very cool.
 export interface Draggable {
   dragStartHandler(event: DragEvent): void;
   dragEndHandler(event: DragEvent): void;
