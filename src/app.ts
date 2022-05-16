519dda789f8b810be18b868adf185bf87d9d7b43
adamhinton <adam.d.hinton@gmail.com>
2022-05-15 23:46:54 -0400

    del unneeded imports, will add to other files instead

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
 //you add three slashes to import from namespace
 //write self closing xml tag and file name as reference path
-/// <reference path = "models/drag-drop.ts"/>
-///<reference path = "models/project.ts"/>
-///<reference path = "state/project-state.ts"/>
-///<reference path = "decorators/autobind.ts"/>
 ///<reference path = "components/project-input.ts"/>
 ///<reference path = "components/project-list.ts"/>
-// This means we tell TS where to find this info
 
 namespace App {
   new ProjectInput();
